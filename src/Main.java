55566906e88c91befe57d1990a9d22848f5a3503
DanielaAgudelo13 <daniela13agudelo@hotmail.com>
2021-05-21 22:02:20 -0500

    Inicio

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("Principal");
+	}
+
+	@Override
+	public void settings() {
+		size(500,500);
+	}	
